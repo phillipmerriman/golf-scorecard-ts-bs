d0636fcc69429156d0c9fc2221950bd4896a7d86
Phillip Merriman <phillipmerriman@gmail.com>
2024-01-19 16:06:59 -0600

    removed unnecessary tests

--- tests/navbar.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('it has a Home link', async ({ page }) => {
-    await page.goto('/');
-    const homeLink = page.getByRole('link', { name: 'Home' });
-    await homeLink.click();
-    expect(page).toHaveURL('/');
-});
