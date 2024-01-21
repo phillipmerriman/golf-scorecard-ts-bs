d32de7cb9bdae09ee7bf3dfe6cfe0f38bf612292
Phillip Merriman <phillipmerriman@gmail.com>
2024-01-21 10:25:59 -0600

    increased timeout in yml file, added webServer.port in config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -85,6 +85,7 @@ export default defineConfig({
     webServer: {
         command: 'npm run dev',
         url: 'http://127.0.0.1:5173',
+        port: 5173,
         // reuseExistingServer: true,
         reuseExistingServer: !process.env.CI,
     },
