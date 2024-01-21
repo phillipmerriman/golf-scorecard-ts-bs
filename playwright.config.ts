54e10c3030e9047dc0899a96df3f461b5957861e
Phillip Merriman <phillipmerriman@gmail.com>
2024-01-21 10:03:45 -0600

    updated webServer.port

--- playwright.config.ts
+++ playwright.config.ts
@@ -90,7 +90,8 @@ export default defineConfig({
     // },
     webServer: {
         command: 'npm run dev',
-        url: 'http://127.0.0.1:3000',
+        // url: 'http://127.0.0.1:3000',
+        port: 4173,
         reuseExistingServer: !process.env.CI,
     },
 });
