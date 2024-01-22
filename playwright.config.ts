9d80e0baa7b1098f4ce0d0302b78c6e1c201fd68
Phillip Merriman <phillipmerriman@gmail.com>
2024-01-22 10:02:01 -0600

    removing workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,6 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        // baseURL: 'http://127.0.0.1:4173',
         baseURL: 'http://127.0.0.1:5173',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -84,16 +83,8 @@ export default defineConfig({
     /* Run your local dev server before starting the tests */
     webServer: {
         command: 'npm run dev',
-        url: 'http://0.0.0.0:5173',
-        // port: 5173,
-        // reuseExistingServer: true,
+        url: 'http://127.0.0.1:5173',
         reuseExistingServer: !process.env.CI,
         timeout: 120 * 1000,
     },
-    // webServer: {
-    //     command: 'npm run dev',
-    //     // url: 'http://127.0.0.1:3000',
-    //     port: 4173,
-    //     reuseExistingServer: !process.env.CI,
-    // },
 });
