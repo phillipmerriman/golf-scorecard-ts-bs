864a9f946e7f0a0a1311a06588c6275d1b4742f2
Phillip Merriman <phillipmerriman@gmail.com>
2024-01-22 08:34:59 -0600

    updated webServer url to 0.0.0.0 to accept connection from any ip address

--- playwright.config.ts
+++ playwright.config.ts
@@ -84,7 +84,7 @@ export default defineConfig({
     /* Run your local dev server before starting the tests */
     webServer: {
         command: 'npm run dev',
-        url: 'http://localhost:5173',
+        url: 'http://0.0.0.0:5173',
         // port: 5173,
         // reuseExistingServer: true,
         reuseExistingServer: !process.env.CI,
